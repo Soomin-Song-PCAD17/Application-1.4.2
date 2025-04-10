f0639da87e9b56846c0807bb5951c7c0dc797493
Soomin Song <song.soomin.418@outlook.com>
2025-04-10 10:26:43 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿var student1 = new Student(1, "Soomin", "Song", 'A');
+Console.WriteLine(student1);
+
+class Student
+{
+    public Student(int studentId, string studentFname, string studentLname, char studentGrade)
+    {
+        StudentId = studentId;
+        StudentFname = studentFname;
+        StudentLname = studentLname;
+        StudentGrade = studentGrade;
+    }
+    private int studentId;
+    private string studentFname;
+    private string studentLname;
+    private char studentGrade;
+
+    public int StudentId
+    {
+        get { return studentId; }
+        set { studentId = value; }
+    }
+    public string StudentFname
+    {
+        get { return studentFname; }
+        set { studentFname = value; }
+    }
+    public string StudentLname
+    {
+        get { return studentLname; }
+        set { studentLname = value; }
+    }
+    public char StudentGrade
+    {
+        get { return studentGrade; }
+        set { studentGrade = value; }
+    }
+
+    public override string ToString() => $"{StudentLname}, {StudentFname} ({StudentId}): {StudentGrade}";
+}
