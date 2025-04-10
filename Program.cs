481042d1b0f3d81e964bb83e2044329d7e0c6fd9
Soomin Song <d.song.149@gmail.com>
2025-04-10 15:20:12 -0700

    ClassRoom added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿var student1 = new Student(1, "Soomin", "Song", 'A');
-Console.WriteLine(student1);
+﻿using StudentManagementSystem;
+var student1 = new Student(1, "Soomin", "Song", 'A');
+var student2 = new Student(2, "Bar", "Foo", 'B');
+// Console.WriteLine(student1);
+
+var math101 = new ClassRoom(1101, "Math 101", 30);
+math101.AddStudent(student1);
+math101.AddStudent(student2);
+Console.WriteLine(math101);
+
+var cmps101 = new ClassRoom(2101, "CMPS 101", 20);
+Console.WriteLine(cmps101);
